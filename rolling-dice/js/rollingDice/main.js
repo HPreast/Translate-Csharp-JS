e79d5e4dc5cf66df2ed56ebc4e59a1e42db01a44
HPreast <preasths1@gmail.com>
2021-05-24 14:25:59 -0500

    translated rolling dice from c# to js

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+const roll = () => {
+    let dieValue = Math.floor(Math.random() * 6) + 1;
+    die = dieValue
+    return die
+}
+
+const getRoll = () => {
+    for (let i = 0; i < 10; i++) {
+        let die1 = roll();
+        let die2 = roll();
+        if (die1 === die2) {
+            console.log(`"${die1} + ${die2} = ${die1 + die2} DOUBLES!"`)
+        } else {
+            console.log(`"${die1} + ${die2} = ${die1 + die2}"`)
+        }
+    }
+}
+getRoll();
