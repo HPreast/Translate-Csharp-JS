efb72ba60b5144ef9e657b4488a4661c37ddc448
HPreast <preasths1@gmail.com>
2021-05-24 13:49:09 -0500

    converted Place Names from c# to js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,24 @@
 // Put your code here
+
+const placeNames = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+]
+
+console.log("All Place Names");
+placeNames.map(name => {
+    console.log(name)
+})
+
+const theNames = [];
+
+placeNames.map(name => {
+    if (name.startsWith("The")) {
+        theNames.push(name)
+    }
+})
+console.log("'The' Place Names");
+
+theNames.map(name => {
+    console.log(name)
+})
